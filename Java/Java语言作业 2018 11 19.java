55d374fad9ca3da55e7f67e2eb3f6ed569cc5b8e
PlanC <2386116982@qq.com>
2018-11-24 11:18:23 +0800

    建立README.md

--- Java/Java语言作业 2018 11 19.java
+++ Java/Java语言作业 2018 11 19.java
@@ -3,7 +3,14 @@
 public class /*文件名*/ {
 
 	public static void main() {
-	
+		System.out.println(add(13, 562));
 	}
 	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int unknown() {
+		return 0;
+	}
 }
