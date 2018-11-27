6103e521261d4a9461be06b89b2e83f7045ad779
PlanC <2386116982@qq.com>
2018-11-27 18:36:05 +0800

    更新2018-11-27的Java作业

--- Java/2018 11 27/visiter.java
+++ Java/2018 11 27/visiter.java
@@ -1,6 +1,10 @@
+//由于Java语言的特殊性，这里只写核心代码
+
 public class Visiter {
+
 	String name;
 	int age;
+
 	public void ticket() {
 		if (age <= 18) {
 			System.out.println(name + "小于十八岁，免费票。");
@@ -9,4 +13,5 @@ public class Visiter {
 			System.out.println(name + "大于十八岁，门票费20RMB。");
 		}
 	}
+	
 }
