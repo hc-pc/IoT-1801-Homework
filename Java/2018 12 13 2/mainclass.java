6a6d3ebf2489ba21e7d0c645f29ad9ed59651bf8
PlanC <2386116982@qq.com>
2018-12-14 23:51:49 +0800

    更新2018 12 13第二次作业

---
+++ Java/2018 12 13 2/mainclass.java
@@ -0,0 +1,8 @@
+//由于Java语言的特殊性，这里只写核心代码
+
+public class mainclass {
+	public static void main(String[] args) {
+		Vehicle vehicle1 = new Vehicle("benz", "black");
+		car vehicle2 = new Car("Honda", "red", 2);
+	}
+}
